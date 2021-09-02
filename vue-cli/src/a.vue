e498f58ff2b74db7a9948bf5f93f007ff6270641
gos1004 <nnv711@gmail.com>
2021-09-02 23:04:51 +0900

    vue create vue-cli cd vue-cli npm run serve

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- HTML -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function(){
+
+        }
+    }
+};
+</script>
+<style>
+    /* CSS */
+</style>
